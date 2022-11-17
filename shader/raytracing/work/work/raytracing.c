2032d93f6b3515ad2d7f0efb6acc72e2abd18e2a
CookieCenter <CookieCenter@web.de>
2022-11-17 15:39:40 +0100

    UpgradeTraverse

--- shader/raytracing/work/work/raytracing.c
+++ shader/raytracing/work/work/raytracing.c
@@ -49,13 +49,13 @@ BasicVoxel castRay(ivec2 dir, vec3 origin) {
 	while (len < 100 && hit == false) {
 		vec3 timeMax = { getDistanceNext(cur_pos.x), getDistanceNext(cur_pos.y), 0.0 };
 		vec3 timeMaxLength = { getTimeMaxLen(timeMax.x, unit.x), getTimeMaxLen(timeMax.y, unit.y), 0.0 };
-		if (timeMaxLength.x > timeMaxLength.y) {
-			cur_pos = (vec3) { unit.x * timeMaxLength.x + cur_pos.x, unit.y * timeMaxLength.x  + cur_pos.y, 0.0 };
-			len += timeMaxLength.x * unitLen;
-		}
 		if (timeMaxLength.x < timeMaxLength.y) {
 			cur_pos = (vec3) { unit.x * timeMaxLength.y + cur_pos.x, unit.y * timeMaxLength.y + cur_pos.y, 0.0 };
 			len += timeMaxLength.y * unitLen;
+		}
+		else {
+			cur_pos = (vec3) { unit.x * timeMaxLength.x + cur_pos.x, unit.y * timeMaxLength.x  + cur_pos.y, 0.0 };
+			len += timeMaxLength.x * unitLen;
 		}
 
 		int pos_y = (int)(cur_pos.y);
@@ -67,8 +67,8 @@ BasicVoxel castRay(ivec2 dir, vec3 origin) {
 
 int main() {
     printf("Starting ... \n");
-	voxelMatrix[0][5][0] = (BasicVoxel) { true, (vec4) { 100, 0, 0, 0 } };
-	BasicVoxel test = castRay((ivec2) { 90, 0 }, (vec3) { 0.0, 0.0, 0.0 });
+	voxelMatrix[5][5][0] = (BasicVoxel) { true, (vec4) { 100, 0, 0, 0 } };
+	BasicVoxel test = castRay((ivec2) { 45, 0 }, (vec3) { 0.0, 0.0, 0.0 });
 	printf("Result\n");
 	printFloat(test.color.x);
     return 0;
