40a70821d0c5107c42d25987e8aab24ced399340
CookieCenter <CookieCenter@web.de>
2022-11-24 20:46:51 +0100

    Hate

--- shader/raytracing/work/shader/compute.glsl
+++ shader/raytracing/work/shader/compute.glsl
@@ -5,6 +5,7 @@
 
 # define MAX 100
 # define PI 3.141592654
+# define STEP_LIMIT 10
 
 layout(local_size_x = 16, local_size_y = 16) in;
 
@@ -23,17 +24,16 @@ vec3 getNewPos(vec3 unit, vec3 old_pos, float selected_length) { return vec3(uni
 
 float getTimeMaxLen(float timeMax, float unit) { if (unit != 0.0) { return timeMax / unit; } else { return 0.0; } }
 
-BasicVoxel castRay(ivec2 dir, vec3 origin) {
+BasicVoxel castRay(ivec2 dir, vec3 origin, int step_limit) {
 	vec2 dir_radiant = vec2(roundFloat(dir.x * PI / 180), roundFloat(dir.y * PI / 180));
 	vec3 unit = vec3(roundFloat(cos(dir_radiant.x) * cos(dir_radiant.y)), roundFloat(sin(dir_radiant.y)), roundFloat(sin(dir_radiant.x) * cos(dir_radiant.y)));
 	float unitLen = sqrt(roundFloat(unit.x * unit.x + unit.y * unit.y + unit.z * unit.z));
 
-	bool hit = false;
 	float len = 0;
 	vec3 cur_pos = origin;
 	BasicVoxel voxel = BasicVoxel(false, vec4(0.0, 1.0, 1.0, 0.0));
 	
-	while (false) {
+	for(int iteration = 0; iteration < step_limit; iteration += 1) {
 		vec3 distNextVox = vec3(getDistanceNext(cur_pos.x), getDistanceNext(cur_pos.y), getDistanceNext(cur_pos.z));
 		vec3 lengthCertainDir = vec3(getTimeMaxLen(distNextVox.x, unit.x), getTimeMaxLen(distNextVox.y, unit.y), getTimeMaxLen(distNextVox.z, unit.z));
 
@@ -59,8 +59,7 @@ BasicVoxel castRay(ivec2 dir, vec3 origin) {
 		}
 
 		BasicVoxel currentVoxel = voxelMatrix[int(cur_pos.x)][int(cur_pos.y)][int(cur_pos.z)];
-		if (currentVoxel.valid == true) { hit = true; voxel = currentVoxel; }
-		if (len >= 10) { hit = true; }
+		if (currentVoxel.valid == true) { voxel = currentVoxel; }
 	}
 	
 	return voxel;
@@ -69,7 +68,7 @@ BasicVoxel castRay(ivec2 dir, vec3 origin) {
 
 void main() {
 	voxelMatrix[15][15][15] = BasicVoxel(true, vec4(0, 100, 0, 0));
-	BasicVoxel test = castRay(ivec2(45, 35), vec3(14, 14, 14));
+	BasicVoxel test = castRay(ivec2(45, 35), vec3(14, 14, 14), STEP_LIMIT);
 	vec4 colTest = vec4(0,0,1,0);
-	imageStore(outImage, ivec2(gl_GlobalInvocationID.xy), vec4(0,1,1,0));
+	imageStore(outImage, ivec2(gl_GlobalInvocationID.xy), test.color);
 }
