90c70bebcbe31f156223cc75742733457e4c6e78
CookieCenter <CookieCenter@web.de>
2022-11-25 20:39:11 +0100

    Update

--- shader/raytracing/work/shader/compute.glsl
+++ shader/raytracing/work/shader/compute.glsl
@@ -4,7 +4,8 @@
 
 # define MAX 100
 # define PI 3.141592654
-# define STEP_LIMIT 2
+# define STEP_LIMIT 1000
+# define ACCURACY 100
 
 layout(local_size_x = 16, local_size_y = 16) in;
 
@@ -17,71 +18,56 @@ struct BasicVoxel {
 
 BasicVoxel[16][16][16] voxelMatrix;
 
-float roundFloat(float val) { return round(val * 100) / 100; }
+float roundFloat(float val) { return round(val * ACCURACY) / ACCURACY; }
 float getDistanceNext(float input_float) { return floor(input_float + 1) - input_float; }
-vec3 getNewPos(vec3 unit, vec3 old_pos, float selected_length) { 
-return vec3(unit.x * selected_length + old_pos.x, unit.y * selected_length + old_pos.y, unit.z * selected_length + old_pos.z); }
-
-float getTimeMaxLen(float timeMax, float unit) { if (unit != 0.0) { return timeMax / unit; } else { return 0.0; } }
+float divide(float first, float sec) { if (sec != 0.0) { return first / sec; } else { return first; } }
 
 BasicVoxel castRay(ivec2 dir, vec3 origin, int step_limit) {
-	float alpha = roundFloat(dir.x * PI / 180);
-	float beta = roundFloat(dir.y * PI / 180);
-	vec3 unit = vec3(roundFloat(cos(alpha) * cos(beta)), roundFloat(sin(beta)), roundFloat(sin(alpha) * cos(beta)));
-	
+	vec4 col = vec4(0, 0, 0, 0);
+	float alpha = dir.x * PI / 180;
+	float beta = dir.y * PI / 180;
+	vec3 unit = vec3(cos(alpha) * cos(beta), sin(beta), sin(alpha) * cos(beta));
 	float len = 0;
 	vec3 curPos = origin;
 	BasicVoxel voxel = BasicVoxel(false, vec4(0.0, 0.0, 0.0, 0.0));
-	BasicVoxel[10] test_list;
-	vec4 col = vec4(0, 0, 0, 0);
 	
 	for(int iteration = 0; iteration < step_limit; iteration += 1) {
 		vec3 distNextVox = vec3(getDistanceNext(curPos.x), getDistanceNext(curPos.y), getDistanceNext(curPos.z));
-		vec3 lengthCertainDir = vec3(getTimeMaxLen(distNextVox.x, unit.x), getTimeMaxLen(distNextVox.y, unit.y), getTimeMaxLen(distNextVox.z, unit.z));
+		vec3 lengthCertainDir = vec3(divide(distNextVox.x, unit.x), divide(distNextVox.y, unit.y), divide(distNextVox.z, unit.z));
 
 		if (lengthCertainDir.x < lengthCertainDir.y) {
 			if (lengthCertainDir.x < lengthCertainDir.z) {
-				curPos = getNewPos(unit, curPos, lengthCertainDir.x);
-				len += lengthCertainDir.x;
-				
-				
+				float selectedLen = lengthCertainDir.x;
+				curPos = vec3(curPos.x + distNextVox.y, unit.y * selectedLen + curPos.y, unit.z * selectedLen + curPos.z);
+				len += selectedLen;
 			}	
 			else {
-				curPos = getNewPos(unit, curPos, lengthCertainDir.z);
-				len += lengthCertainDir.z;
-				
-				
+				float selectedLen = lengthCertainDir.z;
+				curPos = vec3(unit.x * selectedLen + curPos.x, unit.y * selectedLen + curPos.y, curPos.z + distNextVox.z);
+				len += selectedLen;
 			} 
 		}
 		else {
 			if (lengthCertainDir.y < lengthCertainDir.z) {
-				curPos = getNewPos(unit, curPos, lengthCertainDir.y);
-				len += lengthCertainDir.y;
-				
-				
+				float selectedLen = lengthCertainDir.y;
+				curPos = vec3(unit.x * selectedLen + curPos.x, curPos.y + distNextVox.y, unit.z * selectedLen + curPos.z);
+				len += selectedLen;
 			}
 			else {
-				curPos = getNewPos(unit, curPos, lengthCertainDir.z);
-				len += lengthCertainDir.z;
-				
-				
+				float selectedLen = lengthCertainDir.z;
+				curPos = vec3(unit.x * selectedLen + curPos.x, unit.y * selectedLen + curPos.y, curPos.z + distNextVox.z);
+				len += selectedLen;
 			}
 		}
 		
-		if (curPos.x < 16) {
-			col = vec4(curPos.x / 10, curPos.y / 10, curPos.z / 10, 0);
-
-		}
 		
-		// BasicVoxel currentVoxel = voxelMatrix[int(curPos.x)][int(curPos.y)][int(curPos.z)];
+		// BasicVoxel currentVoxel = voxelMatrix[int(floor(curPos.x))][int(floor(curPos.y))][int(floor(curPos.z))];
 		// BasicVoxel currentVoxel = voxelMatrix[1][1][1];
-		if (currentVoxel.valid == true) { voxel = currentVoxel; }
+		// if (currentVoxel.valid == true) { voxel = currentVoxel; }
 		
-		// voxel = BasicVoxel(true, col);
-	}
-	
-	if (gl_WorkGroupID.x < 10) {
-		voxel = test_list[int(gl_WorkGroupID.x)];
+		col = vec4(curPos.x / 10, curPos.y / 10, curPos.z / 10, 0);
+		
+		voxel = BasicVoxel(true, col);
 	}
 	
 	return voxel;
@@ -89,8 +75,7 @@ BasicVoxel castRay(ivec2 dir, vec3 origin, int step_limit) {
 
 	
 void main() {
-	voxelMatrix[1][1][1] = BasicVoxel(true, vec4(0, 1, 0, 0));
-	BasicVoxel test = castRay(ivec2(45, 35), vec3(0, 0, 0), STEP_LIMIT);
-	vec4 colTest = vec4(0,0,1,0);
+	voxelMatrix[1][1][1] = BasicVoxel(true, vec4(0, 1, 1, 0));
+	BasicVoxel test = castRay(ivec2(23, 0), vec3(0, 0, 0), STEP_LIMIT);
 	imageStore(outImage, ivec2(gl_GlobalInvocationID.xy), test.color);
 }
