b634c96778b214da2e843e3e75622b875e7c5d66
CookieCenter <CookieCenter@web.de>
2023-04-26 21:25:18 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{io::Write, thread, time::{Instant, Duration}, borrow::BorrowMut};
+use std::{
+    borrow::BorrowMut,
+    io::Write,
+    thread,
+    time::{Duration, Instant},
+};
 
 use ash::vk;
 use env_logger::fmt::{Color, Formatter};
@@ -8,14 +13,19 @@ use log::Record;
 use octree::Octree;
 use pipe::Pipe;
 use uniform::Uniform;
-use winit::{event_loop::{EventLoop, ControlFlow}, event::{WindowEvent, KeyboardInput, Event}, platform::run_return::EventLoopExtRunReturn, dpi::PhysicalPosition};
+use winit::{
+    dpi::PhysicalPosition,
+    event::{Event, KeyboardInput, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    platform::run_return::EventLoopExtRunReturn,
+};
 
-mod pipe;
+mod input;
 mod interface;
 mod octree;
-mod uniform;
-mod input;
+mod pipe;
 mod service;
+mod uniform;
 
 const DEFAULT_STORAGE_BUFFER_SIZE: u64 = 134217728;
 const DEFAULT_UNIFORM_BUFFER_SIZE: u64 = 16384;
@@ -58,24 +68,26 @@ pub struct Pref {
 }
 
 fn main() {
-    let log_format = | buf: &mut Formatter, record: &Record | {
+    let log_format = |buf: &mut Formatter, record: &Record| {
         let mut buf_style = buf.style();
 
-        buf_style
-            .set_color(Color::Yellow)
-            .set_bold(true);
+        buf_style.set_color(Color::Yellow).set_bold(true);
 
         let time = chrono::Local::now().format("%H:%M:%S");
 
-        writeln!(buf, "[ {} {} ] {}", time, buf_style.value(record.level()), record.args(), ) 
-    };  
+        writeln!(
+            buf,
+            "[ {} {} ] {}",
+            time,
+            buf_style.value(record.level()),
+            record.args(),
+        )
+    };
 
-    env_logger::builder()
-        .format(log_format)
-        .init();
+    env_logger::builder().format(log_format).init();
 
     log::info!("Starting Application ...");
-    thread::spawn(| | { loop { } });
+    thread::spawn(|| loop {});
 
     let mut render = Render::get_render();
     render.execute(Instant::now());
@@ -85,7 +97,7 @@ impl Render {
     pub fn get_render() -> Render {
         let event_loop = EventLoop::new();
 
-        let pref = Pref { 
+        let pref = Pref {
             pref_present_mode: vk::PresentModeKHR::IMMEDIATE,
             img_filter: vk::Filter::LINEAR,
             img_scale: 2.0,
@@ -93,22 +105,33 @@ impl Render {
             name: env!("CARGO_PKG_NAME").to_string(),
             engine_name: "Engine".to_string(),
 
-            start_window_size: vk::Extent2D { width: 800, height: 600 },
+            start_window_size: vk::Extent2D {
+                width: 800,
+                height: 600,
+            },
 
             use_render_res: true,
-            render_res: vk::Extent2D { width: 1920, height: 1080 },
+            render_res: vk::Extent2D {
+                width: 1920,
+                height: 1080,
+            },
         };
 
-        let state = RenderState { out_of_date: false, idle: false, frame_time: Duration::ZERO };
+        let state = RenderState {
+            out_of_date: false,
+            idle: false,
+            frame_time: Duration::ZERO,
+        };
 
         let input = Input::new();
         let mut uniform = Uniform::default();
         let mut octree = Octree::default();
         octree.test_scene();
 
+        let interface = Interface::init(&event_loop, &pref);
+        let graphic_pipe = Pipe::init_render(&interface, &mut uniform);
 
-        let interface = Interface::init(&event_loop, &pref, );
-        let graphic_pipe = Pipe::init(&interface, &mut uniform, &octree, );
+        graphic_pipe.
 
         Render {
             state,
@@ -122,30 +145,66 @@ impl Render {
         }
     }
 
-    pub fn execute(&mut self, app_start: Instant, ) {
+    pub fn execute(&mut self, app_start: Instant) {
         self.event_loop
             .borrow_mut()
-            .run_return(| event, _, control_flow | {
-                * control_flow = ControlFlow::Poll;
+            .run_return(|event, _, control_flow| {
+                *control_flow = ControlFlow::Poll;
                 match event {
-                    Event::WindowEvent { event: WindowEvent::KeyboardInput { input: KeyboardInput { virtual_keycode: Some(keycode), state, .. }, .. }, .. } =>
-                        // Handle KeyboardInput
-                        self.input.handle_key_input(&keycode, &state, &self.interface, &mut self.uniform, ),
+                    Event::WindowEvent {
+                        event:
+                            WindowEvent::KeyboardInput {
+                                input:
+                                    KeyboardInput {
+                                        virtual_keycode: Some(keycode),
+                                        state,
+                                        ..
+                                    },
+                                ..
+                            },
+                        ..
+                    } =>
+                    // Handle KeyboardInput
+                    {
+                        self.input.handle_key_input(
+                            &keycode,
+                            &state,
+                            &self.interface,
+                            &mut self.uniform,
+                        )
+                    }
 
-                    Event::WindowEvent { event: WindowEvent::CursorMoved { position, .. }, .. } => {
+                    Event::WindowEvent {
+                        event: WindowEvent::CursorMoved { position, .. },
+                        ..
+                    } => {
                         self.input.handle_mouse_input(position, &mut self.uniform);
                         self.interface.window.set_cursor_visible(false);
-                        self.interface.window.set_cursor_position(PhysicalPosition::new(self.uniform.resolution.x / 2.0, self.uniform.resolution.y / 2.0, )).unwrap();
-                    },
+                        self.interface
+                            .window
+                            .set_cursor_position(PhysicalPosition::new(
+                                self.uniform.resolution.x / 2.0,
+                                self.uniform.resolution.y / 2.0,
+                            ))
+                            .unwrap();
+                    }
 
-                    Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => * control_flow = ControlFlow::Exit,
+                    Event::WindowEvent {
+                        event: WindowEvent::CloseRequested,
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
                     Event::MainEventsCleared =>
-                        // Adjust Surface and Draw
-                        if self.state.out_of_date { 
+                    // Adjust Surface and Draw
+                    {
+                        if self.state.out_of_date {
                             let dim = self.interface.window.inner_size();
                             if dim.width > 0 && dim.height > 0 {
                                 // Not Minimized
-                                self.graphic_pipe.recreate_swapchain(&mut self.interface, &mut self.uniform, &self.pref, );
+                                self.graphic_pipe.recreate_swapchain(
+                                    &mut self.interface,
+                                    &mut self.uniform,
+                                    &self.pref,
+                                );
 
                                 self.state.idle = false;
                                 self.state.out_of_date = false;
@@ -160,20 +219,25 @@ impl Render {
                             // self.graphic_pipe.update_buffer(&self.interface, self.graphic_pipe.octree_buffer_memory, &self.octree.data.clone(), );
 
                             // Update Uniform
-                            self.uniform
-                                .update_uniform(app_start.elapsed());
-                            self.graphic_pipe.uniform_buffer
-                                .update(&self.interface, &[self.uniform], );
-                            
+                            self.uniform.update_uniform(app_start.elapsed());
+                            self.graphic_pipe
+                                .uniform_buffer
+                                .update(&self.interface, &[self.uniform]);
+
                             // Draw and capture FrameTime
                             let start = Instant::now();
-                            self.state.out_of_date = self.graphic_pipe.draw(&self.interface, &self.pref, ).expect("RENDER_FAILED");
+                            self.state.out_of_date = self
+                                .graphic_pipe
+                                .draw(&self.interface, &self.pref)
+                                .expect("RENDER_FAILED");
                             self.state.frame_time = start.elapsed();
 
                             // self.interface.window.set_title(format!("{} - FrameTime {} - Per Second {}", NAME, self.state.frame_time.as_millis(), 1000 / self.state.frame_time.as_millis()).as_str());
-                        },
+                        }
+                    }
 
-                    Event::LoopDestroyed => self.interface.wait_for_gpu().expect("DEVICE_LOST"), _ => (),
+                    Event::LoopDestroyed => self.interface.wait_for_gpu().expect("DEVICE_LOST"),
+                    _ => (),
                 }
             });
     }
